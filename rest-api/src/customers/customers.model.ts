9a7c4b2b22e170ae5f429d36091f92b88cdfab18
Dinyo <dinyo517@gmail.com>
2023-04-04 19:58:59 +0300

    Implement relation with Warehouse model.

--- rest-api/src/customers/customers.model.ts
+++ rest-api/src/customers/customers.model.ts
@@ -2,11 +2,13 @@ import {
   Column,
   DataType,
   DeletedAt,
+  HasMany,
   Model,
   PrimaryKey,
   Table,
   Unique,
 } from 'sequelize-typescript';
+import { Warehouse } from 'src/warehouses/warehouses.model';
 
 @Table
 export class Customer extends Model {
@@ -30,4 +32,7 @@ export class Customer extends Model {
   @DeletedAt
   @Column
   deletedAt?: Date;
+
+  @HasMany(() => Warehouse)
+  warehouse: Warehouse[];
 }
