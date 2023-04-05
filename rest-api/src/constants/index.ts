8a4b723428dc9c83d5c5023a6706ef5430f35af4
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-05 10:35:03 +0300

    Update constants.

--- rest-api/src/constants/index.ts
+++ rest-api/src/constants/index.ts
@@ -1,3 +1,7 @@
 export const HAZARDOUS = 'hazardous';
 export const NON_HAZARDOUS = 'non-hazardous';
 export const UNKNOWN = 'unknown';
+
+export const WAREHOUSE_MIN_SIZE = 10;
+export const PRODUCT_MIN_SIZE = 0.1;
+export const PRODUCT_MIN_PRICE = 0.1;
