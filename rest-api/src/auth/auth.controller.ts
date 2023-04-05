7a8d46ec8bca151a77f592e9d3d8c2a58202f387
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-05 11:50:12 +0300

    Implement basic Signup functionality.

--- rest-api/src/auth/auth.controller.ts
+++ rest-api/src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+
+import { Customer } from 'src/customers/customers.model';
+import { SignupDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: SignupDto): Promise<Customer> {
+    return this.authService.createCustomerAsync(dto);
+  }
 }
--- rest-api/src/auth/auth.service.ts
+++ rest-api/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 
 import { Customer } from 'src/customers/customers.model';
+import { SignupDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,18 @@ export class AuthService {
     @InjectModel(Customer)
     private customerModel: typeof Customer,
   ) {}
+
+  async createCustomerAsync(dto: SignupDto): Promise<Customer> {
+    const customer = await this.customerModel.create({
+      name: dto.name,
+      password: dto.password,
+      email: dto.email,
+    });
+
+    delete customer.dataValues.password;
+    delete customer.dataValues.deletedAt;
+    delete customer.dataValues.updatedAt;
+
+    return customer;
+  }
 }
