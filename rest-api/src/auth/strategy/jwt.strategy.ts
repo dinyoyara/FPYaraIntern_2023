7f4537701dd53927f558e034057bd0f2c95daf09
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-05 15:02:38 +0300

    Implement jwt passport strategy.

---
+++ rest-api/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/sequelize';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { Customer } from '../../customers/customers.model';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    @InjectModel(Customer)
+    private customerModel: typeof Customer,
+    config: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { id: string }) {
+    const user = await this.customerModel.findByPk(payload.id);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...userWithoutPassword } = user.dataValues;
+    return userWithoutPassword;
+  }
+}
