345fe17385794b0cef523fff866736f2906849a0
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-05 16:14:28 +0300

    Import JwtStrategy into providers.

--- rest-api/src/auth/auth.module.ts
+++ rest-api/src/auth/auth.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Customer } from 'src/customers/customers.model';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [SequelizeModule.forFeature([Customer]), JwtModule],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
