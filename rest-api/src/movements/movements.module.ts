3bf3035640c4ded3263ea02a8ec34459cd25a877
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-06 16:56:49 +0300

    Use ModelRef class.

--- rest-api/src/movements/movements.module.ts
+++ rest-api/src/movements/movements.module.ts
@@ -1,19 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 import { Movement } from './movements.model';
 import { MovementsService } from './movements.service';
 import { MovementsController } from './movements.controller';
-import { WarehousesModule } from 'src/warehouses/warehouses.module';
-import { ProductsModule } from 'src/products/products.module';
+import { WarehousesModule } from '../warehouses/warehouses.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Movement]),
-    WarehousesModule,
+    forwardRef(() => WarehousesModule),
     ProductsModule,
   ],
   providers: [MovementsService],
   controllers: [MovementsController],
+  exports: [MovementsService],
 })
 export class MovementsModule {}
--- rest-api/src/warehouses/warehouses.module.ts
+++ rest-api/src/warehouses/warehouses.module.ts
@@ -1,14 +1,21 @@
 import { SequelizeModule } from '@nestjs/sequelize';
 
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { Warehouse } from './warehouses.model';
 import { WarehousesService } from './warehouses.service';
 import { WarehousesController } from './warehouses.controller';
+import { HttpModule } from '@nestjs/axios';
+import { MathService } from 'src/math/math.service';
+import { MovementsModule } from 'src/movements/movements.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Warehouse])],
+  imports: [
+    SequelizeModule.forFeature([Warehouse]),
+    forwardRef(() => MovementsModule),
+    HttpModule,
+  ],
   controllers: [WarehousesController],
-  providers: [WarehousesService],
+  providers: [WarehousesService, MathService],
   exports: [WarehousesService],
 })
 export class WarehousesModule {}
