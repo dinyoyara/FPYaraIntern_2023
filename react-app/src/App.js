c55c7a5b0716e598d3b66e2eb2032a4b1bab8827
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-07 17:18:53 +0300

    Refactoring app.js and index.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global.css';
+import Body from './components/body';
+
 function App() {
-  return <div>App component</div>;
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <Body />
+        </BrowserRouter>
+    );
 }
 
 export default App;
