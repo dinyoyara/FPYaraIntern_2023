2d48a8ffdce3298ef6bc999cba1f12bfc386c089
Dinyo <dinyo517@gmail.com>
2023-04-09 15:42:56 +0300

    Use customer context in header component.

--- react-app/src/components/header/index.js
+++ react-app/src/components/header/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import NavElement from './navElement';
 import StyledHeader from './styles.css';
+import useCustomerContext from '../../context/customer/hook';
 
 const Header = ({ handleNavigation }) => {
-    const logout = () => {
-        console.log('logout');
+    const { customer, logout } = useCustomerContext();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
     };
 
     return (
         <StyledHeader>
-            <NavElement name='Home' click={() => handleNavigation('/')} />
-            <NavElement name='Warehouses' click={() => handleNavigation('/warehouses')} />
-            <NavElement name='Products' click={() => handleNavigation('/products')} />
-            <NavElement name='Movements' click={() => handleNavigation('/movements')} />
-            <NavElement name='logout' click={logout} />
+            {customer ? (
+                <>
+                    <NavElement name='Warehouses' click={() => handleNavigation('/warehouses')} />
+                    <NavElement name='Products' click={() => handleNavigation('/products')} />
+                    <NavElement name='Movements' click={() => handleNavigation('/movements')} />
+                    <NavElement name='logout' click={handleLogout} />
+                </>
+            ) : (
+                <div>Stock application</div>
+            )}
         </StyledHeader>
     );
 };
