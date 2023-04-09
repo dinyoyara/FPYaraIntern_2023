7dab8932d3f47c1e60258a406b8243a4a6a72423
Dinyo <dinyo517@gmail.com>
2023-04-09 15:41:23 +0300

    Use customer context in home component.

--- react-app/src/components/body/home/index.js
+++ react-app/src/components/body/home/index.js
@@ -1,8 +1,14 @@
 import LoginForm from './loginForm';
 import RegisterForm from './registerForm';
+import useCustomerContext from '../../../context/customer/hook';
+import { useState } from 'react';
 
 const Home = () => {
-    return <>{1 ? <LoginForm /> : <RegisterForm />}</>;
+    const [showLogin, setShowLogin] = useState(true);
+    const { customer } = useCustomerContext();
+    console.log(customer);
+    const form = showLogin ? <LoginForm /> : <RegisterForm />;
+    return <>{customer ? <div>Welcome, {customer.name}</div> : form}</>;
 };
 
 export default Home;
