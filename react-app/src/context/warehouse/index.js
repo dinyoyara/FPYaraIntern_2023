600a1614148e59712d2f79588cf278d1766a3537
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-10 16:35:40 +0300

    Implement getAllAsync method.

--- react-app/src/context/warehouse/index.js
+++ react-app/src/context/warehouse/index.js
@@ -5,26 +5,35 @@ import axiosClient from '../../services/axios.service';
 const WarehouseContext = createContext();
 
 function WarehouseProvider({ children }) {
-    const [warehouse, setWarehouse] = useState();
+    const [warehouses, setWarehouses] = useState();
     const [error, setError] = useState();
 
     const createWarehouseAsync = async (name, size, type) => {
         try {
-            const result = await axiosClient.post(`/warehouses`, { name, type, size });
-            setWarehouse(result.data);
+            await axiosClient.post(`/warehouses`, { name, type, size });
+            await getAllAsync();
+            return true;
+        } catch (error) {
+            setError(error.response.data.message);
+            return false;
+        }
+    };
+
+    const getAllAsync = async () => {
+        try {
+            const response = await axiosClient.get(`/warehouses`);
+            setWarehouses(response.data);
         } catch (error) {
             setError(error.response.data.message);
         }
     };
-
-    const getAllAsync = async () => {};
 
     const clearError = () => {
         setError(null);
     };
 
     const valueToShare = {
-        warehouse,
+        warehouses,
         error,
         createWarehouseAsync,
         getAllAsync,
