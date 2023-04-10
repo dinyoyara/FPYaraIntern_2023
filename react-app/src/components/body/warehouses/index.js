824221985d39f54d62189dc0832299c81037b47d
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-10 17:58:13 +0300

    Refactoring Warehouse components.

--- react-app/src/components/body/warehouses/index.js
+++ react-app/src/components/body/warehouses/index.js
@@ -11,6 +11,8 @@ const Warehouses = () => {
     const [inputName, setInputName] = useState('');
     const [inputSize, setInputSize] = useState(100);
     const [selectType, setSelectType] = useState(UNKNOWN);
+    const [editedId, setEditedId] = useState();
+    const [edit, setEdit] = useState(false);
 
     const [formIsValid, setFormIsValid] = useState(true);
 
@@ -21,84 +23,137 @@ const Warehouses = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const handleSubmit = async () => {
-        const result = await createWarehouseAsync(inputName, inputSize, selectType);
-        if (result) {
-            setInputName('');
-            setInputSize(100);
-            setSelectType(UNKNOWN);
-        }
-    };
     const handleOnChange = (event, setter, fieldName) => {
         setter(event.target.value);
         clearError();
         // validateField(fieldName, event.target.value);
     };
 
+    const handleCreate = async () => {
+        const result = await createWarehouseAsync(inputName, inputSize, selectType);
+        if (result) {
+            clearForm();
+        }
+    };
+
     const handleDelete = async (id) => {
         await deleteAsync(id);
     };
 
-    const handleEdit = (id) => {
-        console.log('edit', id);
+    const handleEdit = () => {
+        console.log(inputName, inputSize, selectType, editedId);
+        clearForm();
+        setEdit(false);
     };
 
-    const createWarehouseFormInputs = [
-        {
-            id: 'name',
-            value: inputName,
-            type: 'text',
-            label: 'Name:',
-            height: formInputHeight,
-            onChange: (e) => handleOnChange(e, setInputName, 'name')
-        },
-        {
-            id: 'size',
-            value: inputSize,
-            type: 'number',
-            label: 'Size:',
-            height: formInputHeight,
-            onChange: (e) => handleOnChange(e, setInputSize, 'size')
-        }
-    ];
-
-    const createWarehouseFormSelect = [
-        {
-            id: 'type',
-            defaultValue: selectType,
-            label: 'Type:',
-            onChange: (e) => handleOnChange(e, setSelectType, 'type'),
-            height: formInputHeight,
-            options: [{ name: HAZARDOUS }, { name: NON_HAZARDOUS }, { name: UNKNOWN }]
-        }
-    ];
-
-    const createWarehouseButton = {
-        text: 'Create',
-        type: 'button',
-        active: formIsValid,
-        handleClick: handleSubmit
+    const handleStartEdit = (id) => {
+        const warehouse = warehouses.find((x) => x.id === id);
+        setEdit(true);
+        setInputName(warehouse.name);
+        setInputSize(warehouse.size);
+        setSelectType(warehouse.type);
+        setEditedId(id);
     };
 
-    const labelData = { name: 'name', type: 'type', size: 'size', freeSpace: 'free space' };
+    const handleStopEdit = () => {
+        setEdit(false);
+        clearForm();
+    };
 
-    const dataButtons = [
-        { name: 'delete', onClick: (id) => handleDelete(id) },
-        { name: 'edit', onClick: (id) => handleEdit(id) }
-    ];
+    const clearForm = () => {
+        setInputName('');
+        setInputSize(100);
+        setSelectType(UNKNOWN);
+    };
+
+    const getFormInputs = () => {
+        return [
+            {
+                id: 'name',
+                value: inputName,
+                type: 'text',
+                label: 'Name:',
+                height: formInputHeight,
+                onChange: (e) => handleOnChange(e, setInputName, 'name')
+            },
+            {
+                id: 'size',
+                value: inputSize,
+                type: 'number',
+                label: 'Size:',
+                height: formInputHeight,
+                onChange: (e) => handleOnChange(e, setInputSize, 'size')
+            }
+        ];
+    };
+
+    const getFormSelects = () => {
+        return [
+            {
+                id: 'type',
+                defaultValue: selectType,
+                label: 'Type:',
+                onChange: (e) => handleOnChange(e, setSelectType, 'type'),
+                height: formInputHeight,
+                options: [{ name: HAZARDOUS }, { name: NON_HAZARDOUS }, { name: UNKNOWN }]
+            }
+        ];
+    };
+
+    const getFormButtons = () => {
+        const buttons = [];
+        if (edit) {
+            buttons.push({
+                text: 'Edit',
+                type: 'button',
+                active: formIsValid,
+                handleClick: handleEdit
+            });
+            buttons.push({
+                text: 'Return to Create',
+                type: 'button',
+                active: formIsValid,
+                handleClick: handleStopEdit
+            });
+        } else {
+            buttons.push({
+                text: 'Create',
+                type: 'button',
+                active: formIsValid,
+                handleClick: handleCreate
+            });
+        }
+        return buttons;
+    };
+
+    const getDataLabels = () => {
+        return { name: 'name', type: 'type', size: 'size', freeSpace: 'free space' };
+    };
+
+    const getDataActions = () => {
+        return [
+            { name: 'delete', onClick: (id) => handleDelete(id) },
+            { name: 'edit', onClick: (id) => handleStartEdit(id) }
+        ];
+    };
 
     return (
         <StyledWarehouseScreen>
-            {warehouses ? (
-                <DataContainer labelData={labelData} data={warehouses} dataButtons={dataButtons} title='Warehouses' />
-            ) : null}
             <Form
-                selectsInfo={createWarehouseFormSelect}
-                inputsInfo={createWarehouseFormInputs}
-                buttonInfo={createWarehouseButton}
-                title='Create Warehouse'
+                selectsInfo={getFormSelects()}
+                inputsInfo={getFormInputs()}
+                buttonsInfo={getFormButtons()}
+                title={edit ? 'Edit Warehouse' : 'Create Warehouse'}
                 error={error}
             />
+            {warehouses ? (
+                <DataContainer
+                    labelData={getDataLabels()}
+                    data={warehouses}
+                    dataButtons={getDataActions()}
+                    title='Warehouses'
+                />
+            ) : null}
         </StyledWarehouseScreen>
     );
 };
