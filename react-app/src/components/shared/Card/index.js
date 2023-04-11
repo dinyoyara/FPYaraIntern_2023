c092252e70d3ae9bb74ca8c3e4a0aeb2b1c993ed
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-11 13:49:27 +0300

    Update Card component. Add StyledCardElement.

--- react-app/src/components/shared/Card/index.js
+++ react-app/src/components/shared/Card/index.js
@@ -1,17 +1,41 @@
-import { StyledLink, StyledTitle, StyledError } from '../../styles.css';
+import { StyledLink, StyledError } from '../../styles.css';
 import DataContainer from '../DataContainer';
-import StyledCard from './styles.css';
+import StyledCard, { StyledCardElement, StyledValue } from './styles.css';
 
 const Card = ({ data, width }) => {
+    const getDataLabels = () => {
+        const { id, ...props } = data.products[0];
+        return Object.keys(props);
+    };
+
+    const getProductsData = () => {
+        return data.products.map((x) => {
+            const { id, ...restProduct } = x;
+            return restProduct;
+        });
+    };
     return (
         <>
             {data ? (
                 <StyledCard width={width}>
-                    <StyledTitle>name: {data.name}</StyledTitle>
-                    <StyledTitle>type: {data.type}</StyledTitle>
-                    <StyledTitle>size: {data.size}</StyledTitle>
-                    <StyledTitle>free space: {data.freeSpace}</StyledTitle>
-                    {data.products.length > 0 ? <DataContainer /> : <StyledError>No products</StyledError>}
+                    <StyledCardElement>
+                        name: <StyledValue>{data.name}</StyledValue>
+                    </StyledCardElement>
+                    <StyledCardElement>
+                        type: <StyledValue>{data.type}</StyledValue>
+                    </StyledCardElement>
+                    <StyledCardElement>
+                        size: <StyledValue>{data.size}</StyledValue>
+                    </StyledCardElement>
+                    <StyledCardElement>
+                        free space: <StyledValue>{data.freeSpace}</StyledValue>
+                    </StyledCardElement>
+                    <StyledCardElement>
+                        products: {data.products.length > 0 ? null : <StyledValue>n/a</StyledValue>}
+                    </StyledCardElement>
+                    {data.products.length > 0 ? (
+                        <DataContainer labelData={getDataLabels()} data={getProductsData()} />
+                    ) : null}
                     <StyledLink>view movements</StyledLink>
                 </StyledCard>
             ) : (
--- react-app/src/components/shared/Card/styles.css.js
+++ react-app/src/components/shared/Card/styles.css.js
@@ -5,4 +5,20 @@ const StyledCard = styled('div')`
     display: flex;
     flex-direction: column;
 `;
+
+const StyledCardElement = styled('div')`
+    height: 35px;
+    width: 100%;
+    box-sizing: border-box;
+    border-bottom: 0.5px solid grey;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+const StyledValue = styled('span')`
+    margin-left: auto;
+    font-weight: 700;
+    font-size: 18px;
+`;
 export default StyledCard;
+export { StyledCardElement, StyledValue };
