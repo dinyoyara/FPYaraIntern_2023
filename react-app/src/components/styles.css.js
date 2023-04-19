0526a5d68336fd43f775146b75c418006bebee5c
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-19 11:09:39 +0300

    Update style files.

--- react-app/src/components/styles.css.js
+++ react-app/src/components/styles.css.js
@@ -5,18 +5,29 @@ const StyledTitle = styled('div')`
     font-size: 20px;
     text-align: center;
 `;
+
 const StyledError = styled('div')`
     width: 100%;
     font-size: 14px;
     text-align: center;
     color: red;
 `;
+
 const StyledLink = styled('div')`
-    margin-bottom: 30px;
     width: 100%;
     font-size: 14px;
     text-align: center;
     color: blue;
     cursor: pointer;
 `;
-export { StyledTitle, StyledError, StyledLink };
+
+const StyledFieldContainer = styled('div')`
+    margin: 8px 0;
+    height: ${(p) => p.height};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    font-size: 1rem;
+`;
+
+export { StyledTitle, StyledError, StyledLink, StyledFieldContainer };
