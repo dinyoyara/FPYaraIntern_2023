498c574f5236d6552ad1eef3fef5c5443b3fcefc
dinyoyara <dinyo.dimitrov@yara.com>
2023-04-20 17:53:18 +0300

    Add Errors service in Products service.

--- rest-api/src/products/products.module.ts
+++ rest-api/src/products/products.module.ts
@@ -4,11 +4,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Product } from './products.model';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { ErrorsService } from 'src/errors/errors.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Product])],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, ErrorsService],
   exports: [ProductsService],
 })
 export class ProductsModule {}
